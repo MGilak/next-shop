17464c8a8c688d8eecbfe553cef3768943e0fc23
Mahmoud Gilak <mahmoud.gilak@yahoo.com>
2023-12-15 20:38:01 +0330

    responsive cart page

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -43,7 +43,7 @@ const Cart = () => {
     <>
       <Layout>
         <div className="mb-20">
-          {session?.user ? (
+          {/* {session?.user ? (
             <div className="container mx-auto flex">
               <Link href="http://localhost:3000/api/auth/signout">
                 <button className="bg-red-500 text-white p-4 rounded-lg">
@@ -51,60 +51,66 @@ const Cart = () => {
                 </button>
               </Link>
             </div>
-          ) : null}
+          ) : null} */}
 
           {carts.length > 0 ? (
-            <section className="mt-10 container mx-auto flex items-start gap-2">
+            <section className="mt-10 container mx-auto flex md:flex-row flex-col items-start gap-2 sm:px-0 px-4">
               {/* right */}
               <div
-                className={`w-[75%]  flex flex-col gap-2 rounded-lg ${
+                className={`md:w-[75%] w-[100%]  flex flex-col gap-2 rounded-lg ${
                   carts.length > 0 && "border-[1px] "
                 }`}
               >
                 {carts.map((item) => (
-                  <div className="flex justify-around items-center">
+                  <div className="flex justify-around items-center lg:gap-0 gap-2 px-3 lg:px-0 py-3 sm:py-0">
                     <div>
-                      <img className="w-44" src={item.path} alt="" />
+                      <img className="w-44" src={item.path} alt={item.name} />
                     </div>
 
                     <div>
-                      <h2 className="text-[15px] font-bold">{item.name}</h2>
+                      <h2 className="text-[13px] lg:text-[15px] font-bold">
+                        {item.name}
+                      </h2>
                     </div>
 
-                    <div className="flex gap-2 items-center">
-                      <span
-                        onClick={() => increase(item)}
-                        className="text-2xl select-none cursor-pointer border-[1px] border-green-500 w-8 justify_center rounded-lg hover:bg-white bg-green-600 text-white hover:text-black my_transition"
-                      >
-                        +
-                      </span>
-                      <span
-                        className="w-12 h-10 border-0 outline-none ring-0 text-center justify_center pt-1 font-bold"
-                        type="number"
-                      >
-                        {item.qnt}
-                      </span>
-                      <span
-                        onClick={() => decreaseItem(item)}
-                        className="text-2xl select-none cursor-pointer border-[1px] border-red-400 w-8 justify_center rounded-lg hover:bg-white bg-red-600 text-white hover:text-black my_transition"
-                      >
-                        -
-                      </span>
-                    </div>
-
-                    <div>
-                      {item.off !== 0 && (
-                        <div className="flex items-center gap-1 text-sm mb-2 line-through">
-                          <span>{replace(toFarsiNumber(item.price))}</span>
-                        </div>
-                      )}
-                      <div className="flex items-center gap-1">
-                        <span className="font-bold">
-                          {replace(
-                            toFarsiNumber(finalPrice(item.price, item.off))
-                          )}
+                    <div className="flex sm:gap-7 sm:flex-row flex-col">
+                      <div className="flex lg:gap-2 items-center">
+                        <span
+                          onClick={() => increase(item)}
+                          className="text-2xl select-none cursor-pointer border-[1px] border-green-500 w-7 h-7 lg:w-8 lg:h-8 justify_center rounded-lg hover:bg-white bg-green-600 text-white hover:text-black my_transition"
+                        >
+                          +
                         </span>
-                        <span className="text-xs">تومان</span>
+                        <span
+                          className="w-12 h-10 border-0 outline-none ring-0 text-center justify_center pt-1 font-bold"
+                          type="number"
+                        >
+                          {item.qnt}
+                        </span>
+                        <span
+                          onClick={() => decreaseItem(item)}
+                          className="text-2xl select-none cursor-pointer border-[1px] border-red-400  w-7 h-7 lg:w-8 lg:h-8 justify_center rounded-lg hover:bg-white bg-red-600 text-white hover:text-black my_transition"
+                        >
+                          -
+                        </span>
+                      </div>
+
+                      <div>
+                        {item.off !== 0 && (
+                          <div className="flex  items-center gap-1 text-sm mb-2 line-through">
+                            <span className=" w-full sm:text-right text-center">
+                              {replace(toFarsiNumber(item.price))}
+                            </span>
+                          </div>
+                        )}
+                        <div className="flex justify-center sm:justify-start items-center gap-1">
+                          <span className="font-bold sm:text-[16px] text-[13px]">
+                            {replace(
+                              toFarsiNumber(finalPrice(item.price, item.off))
+                            )}
+                          </span>
+                          <span className="text-xs">تومان</span>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -112,23 +118,27 @@ const Cart = () => {
               </div>
 
               {/* left */}
-              <div className="border-[1px] rounded-lg w-[25%] py-5 px-6 bg-slate-50 dark:bg-[#0f172a] gap-6">
+              <div className="border-[1px] rounded-lg md:w-[25%] w-[100%]  py-5 px-6 bg-slate-50 dark:bg-[#0f172a] gap-6">
                 <div className="flex items-center justify-between mb-6">
-                  <span>تعداد کالاها</span>
-                  <span className="font-bold">
+                  <span className="text-[13px] lg:text-[16px]">
+                    تعداد کالاها
+                  </span>
+                  <span className="font-bold text-[14px] lg:text-[16px]">
                     {replace(toFarsiNumber(carts.length))}
                   </span>
                 </div>
 
                 <div className="flex justify-between mb-10">
-                  <span>جمع سبد خرید</span>
-                  <span className="font-bold">
+                  <span className="text-[13px] lg:text-[16px]">
+                    جمع سبد خرید
+                  </span>
+                  <span className="font-bold text-[15px] lg:text-[16px]">
                     {replace(toFarsiNumber(totalPay()))}
                   </span>
                 </div>
 
                 <div className="w-full flex justify-center">
-                  <button className="w-[80%] bg-[#ef4056] rounded-lg text-white py-3">
+                  <button className="w-[80%] bg-[#ef4056] rounded-lg text-white py-2 lg:py-3 text-[15px] lg:-[text-16px]">
                     <Link href="/">ثبت سفارش</Link>
                   </button>
                 </div>
